559d31a44e7878204d753157b3fdc0b055d307ed
Jerome Blanchard <blanchard.jerome@gmail.com>
2014-01-15 15:32:59 +0100

    delay added to fix race condition issue

--- lib/sunspot/queue/sidekiq/backend.rb
+++ lib/sunspot/queue/sidekiq/backend.rb
@@ -3,15 +3,17 @@ require "sunspot/queue/sidekiq/removal_job"
 
 module Sunspot::Queue::Sidekiq
   class Backend
-    attr_reader :configuration
+    attr_reader :configuration, :delay_for
 
-    def initialize(configuration = Sunspot::Queue.configuration)
+    def initialize(configuration = Sunspot::Queue.configuration, delay_for = 5.seconds)
       @configuration = configuration
+      @delay_for = delay_for
     end
 
     # Job needs to include Sidekiq::Worker
     def enqueue(job, klass, id)
-      job.perform_async(klass.to_s, id)
+      job.perform_in(@delay_for, klass.to_s, id)
+      # job.perform_async(klass.to_s, id)
     end
 
     def index(klass, id)
